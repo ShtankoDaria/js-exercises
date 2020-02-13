771a5e7fb38ee0668dfe2723b018eab4ece13c9f
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-13 11:42:50 +0100

    Done

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -6,9 +6,9 @@ var result = s('code', 'your');
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
-// - How can this function be improved?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. - It concatinates w2 to w1
+// - How many parameters does the function take? 2
+// - What is the function name? s
+// - Where is the function called? with what parameters? it is called and assigned to var result, 2
+// - What does the function return? 2 concatinated words codeyour
+// - How can this function be improved? should add space
