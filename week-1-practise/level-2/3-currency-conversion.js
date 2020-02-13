41f77d46015f0c8fea5b48adf2db8f9b56a2cd29
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-13 13:58:48 +0100

    done

--- week-1-practise/level-2/3-currency-conversion.js
+++ week-1-practise/level-2/3-currency-conversion.js
@@ -5,7 +5,8 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(price) {
+  return (price / 0.92)}
 
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
