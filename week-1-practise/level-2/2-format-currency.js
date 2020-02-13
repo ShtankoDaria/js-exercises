8e9ff898764ff92bd261989cf44043dd2326e3ed
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-13 13:46:40 +0100

    Done

--- week-1-practise/level-2/2-format-currency.js
+++ week-1-practise/level-2/2-format-currency.js
@@ -6,8 +6,10 @@
   Write a function that transforms numbers into the format £0.00
 */
 
-function formatCurrency() {}
-
+function formatCurrency(price)  {
+  return Number(price.toFixed(2));
+  
+  }
 /* DO NOT EDIT BELOW THIS LINE */
 /* --------------------------- */
 
