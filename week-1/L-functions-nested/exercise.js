bb6a878e9a77497e602c7f89375d8a51861a8d52
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-13 11:18:34 +0100

    Solution

--- week-1/L-functions-nested/exercise.js
+++ week-1/L-functions-nested/exercise.js
@@ -3,3 +3,9 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+console.log(("Hello" + " " + mentor1).toUpperCase());
+console.log(("Hello" + " " + mentor2).toUpperCase());   
+console.log(("Hello" + " " + mentor3).toUpperCase());
+console.log(("Hello" + " " + mentor4).toUpperCase());   
+console.log(("Hello" + " " + mentor5).toUpperCase());
