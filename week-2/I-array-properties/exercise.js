94912585b65ba9a74da8e14cf7db1f823bf9a679
ShtankoDaria <59530782+ShtankoDaria@users.noreply.github.com>
2020-02-22 09:12:53 +0100

    Update exercise.js

--- week-2/I-array-properties/exercise.js
+++ week-2/I-array-properties/exercise.js
@@ -6,14 +6,20 @@
 */
 
 function isEmpty(arr) {
-  return arr = []; // complete this statement
+  if (arr.length == 0) {
+return true;
+}else{
+  return false;
+}
+  // complete this statement
 }
 
-/* 
+/*
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 var numbers = [1, 2, 3];
 var names = [];
+
 
 console.log(isEmpty(numbers));
 console.log(isEmpty(names));
